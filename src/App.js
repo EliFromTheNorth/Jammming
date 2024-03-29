0b6d6ef73612be01b054596e789f1ce0b1ec965a
Eliska Knop Kostkova <e.knopkostkova@gmail.com>
2024-03-29 05:43:34 +0100

    change to index and app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Hello
-          Edit <code>src/App.js</code> and save to reload.
+          Edit HElllllo <code>src/App.js</code> and save to reloadddddd.
         </p>
         <a
           className="App-link"
